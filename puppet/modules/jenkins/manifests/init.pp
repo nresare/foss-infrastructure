3c239e953e88e1e744c0fa917bc0e0c39d0be9f3
Noa Resare <noa@spotify.com>
2013-12-19 14:26:53 +0100

    Added jenking maven plugin compatible with upstream ubuntu jenkins

--- puppet/modules/jenkins/manifests/init.pp
+++ puppet/modules/jenkins/manifests/init.pp
@@ -3,4 +3,11 @@ class jenkins {
     "jenkins":
       ensure => installed,
   }
+
+  file {
+    "/var/lib/jenkins/plugins/maven-plugin-1.509.2.hpi":
+      ensure => file,
+      source => "puppet:///modules/jenkins/maven-plugin-1.509.2.hpi",
+      require => Package["jenkins"],
+  }
 }
