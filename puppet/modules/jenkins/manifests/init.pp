38d147498edacd3290889a7931443526b3674bd8
Noa Resare <noa@spotify.com>
2014-01-07 16:12:03 +0100

    Make sure that jenkins plugin dir is created before we install the plugin

--- puppet/modules/jenkins/manifests/init.pp
+++ puppet/modules/jenkins/manifests/init.pp
@@ -5,11 +5,20 @@ class jenkins {
   }
 
   file {
+   "/var/lib/jenkins/plugins":
+     ensure => directory,
+     owner => jenkins,
+     group => jenkins,
+     mode => 755,
+     require => Package["jenkins"],
+  }
+
+  file {
     # this file is downloaded from
     # http://updates.jenkins-ci.org/download/plugins/maven-plugin/
     "/var/lib/jenkins/plugins/maven-plugin-1.509.2.hpi":
       ensure => file,
       source => "puppet:///modules/jenkins/maven-plugin-1.509.2.hpi",
-      require => Package["jenkins"],
+      require => File["/var/lib/jenkins/plugins"],
   }
 }
