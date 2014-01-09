1bb92d6ebd11a40413d2b60638db1433f7921f87
Noa Resare <noa@spotify.com>
2014-01-09 13:22:45 +0100

    formatting

--- puppet/modules/jenkins/manifests/init.pp
+++ puppet/modules/jenkins/manifests/init.pp
@@ -6,13 +6,13 @@ class jenkins {
 
 
   $plugins = {
-    'git'             => { plugin_version => '2.0' },
-    'ssh-credentials' => { plugin_version => '1.6' },
-    'scm-api'         => { plugin_version => '0.2' },
-    'credentials'     => { plugin_version => '1.9.4' },
-    'javadoc'         => { plugin_version => '1.1' },
-    'mailer'          => { plugin_version => '1.8' },
-    'maven-plugin'    => { plugin_version => '1.509.2' },
+    'git'                    => { plugin_version => '2.0' },
+    'ssh-credentials'        => { plugin_version => '1.6' },
+    'scm-api'                => { plugin_version => '0.2' },
+    'credentials'            => { plugin_version => '1.9.4' },
+    'javadoc'                => { plugin_version => '1.1' },
+    'mailer'                 => { plugin_version => '1.8' },
+    'maven-plugin'           => { plugin_version => '1.509.2' },
     'scm-sync-configuration' => { plugin_version => '0.0.7.3' },
     'subversion'             => { plugin_version => '1.54' },
   }
